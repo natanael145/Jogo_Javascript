f004963df5aa13e4de07e3da92b9284b7fe69b44
Natanael Fernandes Toscano Araújo <natanael145@A205-09L.imd.local>
2018-05-29 14:47:28 -0300

    upgrade in js

--- BOTCLICKER/js/game.js
+++ BOTCLICKER/js/game.js
@@ -1,5 +1,6 @@
 var qtcircuitos = 0;
 var qtfios = 0;
+
 function clickitens(){
     qtfios++;
     qtcircuitos++;
@@ -7,4 +8,30 @@ function clickitens(){
     document.getElementById('qtcircuitos').innerHTML = qtcircuitos;
 }
 document.getElementById("pointclick").onclick = function() {clickitens()};
-document.getElementById("clickfig").onclick = function () {clickitens()};
+
+function buyUpgrade(price1,price2,id){
+	qtfios1 = qtfios - price1;
+	qtcircuitos1 = qtcircuitos - price2;
+	if (qtfios1 < 0 && qtcircuitos1 < 0) {
+		myFunctionFailed();
+		return;
+	}
+	qtfios = qtfios1;
+	qtcircuitos = qtcircuitos1;
+	document.getElementById('qtfios').innerHTML = parseInt(qtfios);
+    document.getElementById('qtcircuitos').innerHTML = parseInt(qtcircuitos);
+    myFunctionSucess();
+    document.getElementById(id).outerHTML = "";
+}
+document.getElementByClassName('buttonbuy').onclick = function() {buyUpgrade()};
+
+function myFunctionSucess() {
+    var x = document.getElementById("snackbar-sucess");
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+function myFunctionFailed() {
+    var x = document.getElementById("snackbar-failed");
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
