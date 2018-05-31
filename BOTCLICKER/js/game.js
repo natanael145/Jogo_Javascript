1b410790b0dcc87764893aa4511941dfb04c34cc
Natanael Fernandes <natan-fernandes1@hotmail.com>
2018-05-31 16:41:39 -0300

    new upgrades model

--- BOTCLICKER/js/game.js
+++ BOTCLICKER/js/game.js
@@ -1,22 +1,22 @@
-    //VARIABLES
-
+//VARIABLES
 var qtcircuitos = 0;
 var qtfios = 0;
 var qtenergia = 0;
 var energiasec = 0;
 var timeID;
 var clickQT = 1;
-var out = false;
+var aux = 3;
 
-// ==== ADD CIRCUITS AND THREADS WHEN CLICKED ==============
-function setNoClickFlip(){
+// ========= BACK TO NORMAL IMG ONMOUSEDOWN ==============
+function setNoClickFlip() {
     var element = document.getElementById("img-click");
-    element.setAttribute("src","imgs/clickFlip.png");
+    element.setAttribute("src", "imgs/clickFlip.png");
 }
-function clickitens(){
+// ==== ADD CIRCUITS AND THREADS WHEN CLICKED ==============
+function clickitens() {
     myFunctionAdd();
     var element = document.getElementById("img-click");
-    element.setAttribute("src","imgs/noclickFlip.png");
+    element.setAttribute("src", "imgs/noclickFlip.png");
     qtfios = qtfios + clickQT;
     qtcircuitos = qtcircuitos + clickQT;
     document.getElementById('qtfios').innerHTML = qtfios;
@@ -26,26 +26,26 @@ function clickitens(){
 //===========================================================
 
 //========== BUY UPGRADE AND APPLY IT ON game.html ======================
-function buyUpgrade(price1,price2,price3,id){
-    if (qtfios >= price1 && qtcircuitos >= price2 && qtenergia >=price3) {
-		var qtfios1 = qtfios - price1;
+function buyUpgrade(price1, price2, price3, id) {
+    if (qtfios >= price1 && qtcircuitos >= price2 && qtenergia >= price3) {
+        var qtfios1 = qtfios - price1;
         var qtcircuitos1 = qtcircuitos - price2;
         var qtenergia1 = qtenergia - price3;
-	    qtfios = qtfios1;
+        qtfios = qtfios1;
         qtcircuitos = qtcircuitos1;
         qtenergia = qtenergia1;
-	    document.getElementById('qtfios').innerHTML = parseInt(qtfios);
+        document.getElementById('qtfios').innerHTML = parseInt(qtfios);
         document.getElementById('qtcircuitos').innerHTML = parseInt(qtcircuitos);
         document.getElementById('qtenergia').innerHTML = parseInt(qtenergia);
         myFunctionSucess();
-        appUpgrades(id,qtenergia);
+        appUpgrades(id, qtenergia);
         document.getElementById(id).outerHTML = "";
+        
     }
-    else{
+    else {
         myFunctionFailed();
         return;
     }
-    
 }
 
 //===========================================================
@@ -54,46 +54,47 @@ function buyUpgrade(price1,price2,price3,id){
 function myFunctionSucess() {
     var x = document.getElementById("snackbar-sucess");
     x.className = "show";
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
 }
 function myFunctionFailed() {
     var x = document.getElementById("snackbar-failed");
     x.className = "show";
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
 }
 function myFunctionAdd() {
     var x = document.getElementById("snackbar-add");
     x.className = "show";
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
 }
 //===========================================================
-function setEnergy(energyAux){
+function setEnergy(energyAux) {
     qtenergia = energyAux;
 }
-function getEnergy(){
+function getEnergy() {
     return qtenergia;
 }
 //====================== CHANGE ENERGY PER SEC ==========================
-function addEnergy (qtenergiax,id) {
-        qtenergiax =  qtenergiax + energiasec;
-        document.getElementById('qtenergia').innerHTML = qtenergiax;
-        setEnergy(qtenergiax);
-        timeID = setTimeout("addEnergy("+qtenergiax+")",1000);
+function addEnergy(qtenergiax, id) {
+    qtenergiax = qtenergiax + energiasec;
+    document.getElementById('qtenergia').innerHTML = qtenergiax;
+    setEnergy(qtenergiax);
+    timeID = setTimeout("addEnergy(" + qtenergiax + ")", 1000);
 }
 //===========================================================
 
 // =========== VERIFY AN UPGRADE AND APPLY IT ==================
-function appUpgrades(id,qtenergiaB){
+function appUpgrades(id, qtenergiaB) {
     if (id == 'up1') {
         energiasec = 1;
         document.getElementById('qtenergiasec').innerHTML = energiasec;
-        addEnergy(qtenergiaB,id);
+        addEnergy(qtenergiaB, id);
+        document.getElementById("upgrades").appendChild(up3);
     }
-    else if (id == 'up2'){
+    else if (id == 'up2') {
         clearTimeout(timeID);
         energiasec = 6;
         document.getElementById('qtenergiasec').innerHTML = energiasec;
-        addEnergy(qtenergiaB,id);
+        addEnergy(qtenergiaB, id);
         clickQT = 2;
     }
 }
---
+++ BOTCLICKER/js/upgrades.js
@@ -0,0 +1,38 @@
+// ========================= UPGRADE 3 =====================================================
+var up3 = document.createElement("div"); 
+up3.className = "upgrade";
+up3.id = 'up3';
+var contentUp3 = document.createElement("p");
+contentUp3.className = "p-font-upgrade";
+contentUp3.innerHTML = "Recolha os circuitos e placas-mãe dos velhos fliperamas para melhorar sua AI(Inteligência Artificial)!<br>(CLICKS 2x)<br>(ENERGY 2x)"
+up3.appendChild(contentUp3);
+var threads = document.createElement("p");
+threads.className = "p-font-priceUpgrade";
+threads.innerHTML = "THREADS : "
+var circuits = document.createElement("p");
+circuits.className = "p-font-priceUpgrade";
+circuits.innerHTML = "CIRCUITS : ";
+var energy = document.createElement("p");
+energy.className = "p-font-priceUpgrade";
+energy.innerHTML = "ENERGY : ";
+var price1Up3 = document.createElement("span");
+price1Up3.className = "priceA1";
+price1Up3.innerHTML = 700;
+var price2Up3 = document.createElement("span");
+price2Up3.className = "priceB1";
+price2Up3.innerHTML = 875;
+var price3Up3 = document.createElement("span");
+price3Up3.className = "priceC1";
+price3Up3.innerHTML = 750;
+var buttonbuy = document.createElement("img");
+buttonbuy.className = "buttonbuy";
+buttonbuy.setAttribute("src","imgs/upgradebutton.png");
+buttonbuy.setAttribute("onclick","buyUpgrade(700,875,750,'up3')");
+up3.appendChild(threads);
+up3.appendChild(circuits);
+up3.appendChild(energy);
+threads.appendChild(price1Up3);
+circuits.appendChild(price2Up3);
+energy.appendChild(price3Up3)
+up3.appendChild(buttonbuy);
+//==========================================================================================
